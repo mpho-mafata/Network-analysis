b4fdb9e6a540d3c1dde184b36b42b7ec503d201f
Mpho Mafata <mafata@sun.ac.za>
2024-01-05 12:43:39 +0200

    Create network_analysis.py

---
+++ network_analysis.py
@@ -0,0 +1,10 @@
+# import the necessary libraries
+import networkx
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# read the data
+df = pd.read_excel("/Users/mphomafata/Documents/GitHub/Network-analysis/network-example-data/orgs_rankings_top.xlsx")
+
+print(df)
+exit()
