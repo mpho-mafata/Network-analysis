908c94ef358ee371601f443264c43e19eaa6b2ac
Mpho Mafata <mafata@sun.ac.za>
2024-01-10 10:50:24 +0200

    added data wrangling with python.

--- network_analysis.py
+++ network_analysis.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-# read the data
+# read the SQL wrangled data
 dataset = pd.read_excel("/Users/mphomafata/Documents/GitHub/Network-analysis/network-example-data/edgelist_orgs_network.xlsx")
 dataset.columns=["org1","org2","weight"]
 dataset['weight'] = ((dataset['weight'])/max(dataset['weight']))*10 # attempt at scaling the edge sizes from zero to one
@@ -16,15 +16,11 @@ node_sizes = pd.DataFrame(pd.concat([node_sizes_1, node_sizes_2]))
 node_sizes['orgs'] = node_sizes.index
 node_sizes = node_sizes.groupby(['orgs']).aggregate({'orgs':'sum'}).reset_index(drop=True)
 
-
-print(node_sizes_1)
-
 # plot the graph
 plt.figure(figsize=(30, 15))
 network_graph = nx.Graph()
 network_graph = nx.from_pandas_edgelist(dataset, source="org1", target="org2", edge_attr ='weight')
 widths = nx.get_edge_attributes(network_graph, name= 'weight')
-
 network_graph = nx.draw(network_graph, with_labels=True, font_size=25, alpha=1.0,
                         node_size=800,
                         node_color=range(len(node_sizes)), # generates random node colours
@@ -35,6 +31,40 @@ network_graph = nx.draw(network_graph, with_labels=True, font_size=25, alpha=1.0
                         edge_color = 'grey'
                         )
 plt.savefig(fname='networkx_graph.svg', dpi=800,
+            bbox_inches="tight", pad_inches=0.0,
+            transparent=True, format="svg")
+
+# wrangle the original dataset
+dataset2 = pd.read_csv("/Users/mphomafata/Documents/GitHub/Network-analysis/network-example-data/orgs_rankings_top.csv",
+                      sep=";",
+                      header=0)
+nodes = dataset2.groupby(["organisation"]).aggregate({'pubs': 'first'}).reset_index()
+edges = dataset2[["ut","organisation"]]
+# join the dataframe with itself based on the unique identifier (UT)
+edges = pd.merge(left=edges,right=edges, how="left", on='ut')
+# then remove the duplicates where org x is the same as org y
+edges = edges.query(" organisation_x != organisation_y ").reset_index(drop = True,inplace=False)
+# now remove the duplicates where org x: orgy is the same as org y: org x
+arrays = edges[["organisation_x"]] + edges[["organisation_y"]]
+
+edges = edges.groupby(["organisation_x","organisation_y"]).aggregate({'ut': 'count'}).reset_index() # now count the unique number of UTs
+edges['ut'] = ((edges['ut'])/max(edges['ut']))*10 # attempt at scaling the edge sizes from zero to one
+
+# plot the graph
+plt.figure(figsize=(30, 15))
+network_graph = nx.Graph()
+network_graph = nx.from_pandas_edgelist(edges, source="organisation_x", target="organisation_y", edge_attr ='ut')
+widths = nx.get_edge_attributes(network_graph, name= 'ut')
+network_graph = nx.draw(network_graph, with_labels=True, font_size=25, alpha=1.0,
+                        node_size=800,
+                        node_color=range(len(nodes)), # generates random node colours
+                        cmap=plt.cm.viridis, # colour map for the nodes
+                        arrows=True,
+                        connectionstyle="arc3, rad=0.20",
+                        width=list(widths.values()),
+                        edge_color = 'grey'
+                        )
+plt.savefig(fname='networkx_graph_2.svg', dpi=800,
             bbox_inches="tight", pad_inches=0.0,
             transparent=True, format="svg")
 plt.show()
