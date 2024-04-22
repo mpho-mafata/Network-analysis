3b0fd9f9a72d1e8cb742643bd053eca77eef7b0e
Mpho Mafata <mafata@sun.ac.za>
2024-04-22 14:04:11 +0200

    Update network_analysis.py

--- network_analysis.py
+++ network_analysis.py
@@ -23,30 +23,31 @@ dataset=dataset.drop_duplicates('sorted').reset_index(drop=True)
 # CALCULATE THE NODE SIZES HERE AFTER FILTERING FOR THE CUT-OFF WEIGHT
 node_sizes_x = dataset.groupby(["target_x"]).aggregate({'weight': 'sum'}).reset_index()
 node_sizes_y = dataset.groupby(["target_y"]).aggregate({'weight': 'sum'}).reset_index()
-node_sizes = pd.merge(left=node_sizes_x,left_on="target_x",
-                      right=node_sizes_y,right_on="target_y",
+node_sizes = pd.merge(left=node_sizes_x, left_on="target_x",
+                      right=node_sizes_y, right_on="target_y",
                       how="outer").fillna(0)
-node_sizes["weight"]=node_sizes["weight_x"]+node_sizes["weight_y"]
+node_sizes["weight"] = node_sizes["weight_x"] + node_sizes["weight_y"]
 node_sizes["weight"] = node_sizes["weight"].astype("int")
 
 # CREATE A RULE FOR THE NODE NAME
-conditions = [(node_sizes["target_x"]==0),
-              (node_sizes["target_x"]!=0)]
+conditions = [(node_sizes["target_x"] == 0),
+              (node_sizes["target_x"] != 0)]
 name = [node_sizes["target_y"],
         node_sizes["target_x"]]
-node_sizes["labels"]=np.select(conditions,name)
-node_sizes = node_sizes[["labels","weight"]]
+node_sizes["labels"] = np.select(conditions, name)
+node_sizes = node_sizes[["labels", "weight"]]
 
 # SCALE THE EDGE WIDTHS FOR VISUALIZATION
-dataset["weight"] = dataset["weight"]/50
+dataset["weight"] = dataset["weight"] / 50
 
 # INITIATE THE GRAPH
 plt.figure(figsize=(10, 10))
 network_graph = nx.Graph()
-network_graph = nx.from_pandas_edgelist(dataset, source="target_x", target="target_y", edge_attr ='weight')
+network_graph = nx.from_pandas_edgelist(dataset, source="target_x", target="target_y", edge_attr='weight')
 
 # CALCULATE SOME NETWORK METRICS
 from networkx.algorithms import approximation as apxa
+
 net_centrality = nx.degree_centrality(network_graph)
 print("The network centrality is " + str(len(net_centrality)))
 network_radius = nx.radius(network_graph)
@@ -57,36 +58,39 @@ net_clusters = apxa.k_components(network_graph)
 print("The number of clusters is " + str(len(net_clusters)))
 
 # SET NODE AND EDGE ATTRIBUTES
-edge_widths = nx.get_edge_attributes(network_graph,"weight")
-new_order = pd.merge(left = pd.DataFrame(network_graph.nodes(data=True)),
+edge_widths = nx.get_edge_attributes(network_graph, "weight")
+new_order = pd.merge(left=pd.DataFrame(network_graph.nodes(data=True)),
                      left_on=0,
-                     right = node_sizes,
+                     right=node_sizes,
                      right_on="labels",
                      how="left")
-node_sizes = new_order[["labels","weight"]]
+node_sizes = new_order[["labels", "weight"]]
 
 # DRAW THE NETWORK GRAPH
-position = nx.spring_layout(network_graph, k=4) # Adjust node distances here
+position = nx.spring_layout(network_graph, k=4)  # Adjust node distances here
 network_graph = nx.draw(network_graph, with_labels=True, font_size=12, alpha=0.75,
-                                node_size = node_sizes["weight"]/1, # adjust the size of nodes here
-                                node_color= range(len(node_sizes)), # random node colours
-                                cmap=plt.cm.Spectral, # colour map for the nodes
-                                arrows=True, # even though this is an undirected network, we add this so we can make the edges curved
-                                connectionstyle="arc3, rad=0.25",
-                                pos=position,
-                                width=list(edge_widths.values()),
-                                edge_color = range(len(dataset))
+                        node_size=node_sizes["weight"] / 1,  # adjust the size of nodes here
+                        node_color=range(len(node_sizes)),  # random node colours
+                        cmap=plt.cm.Spectral,  # colour map for the nodes
+                        arrows=True,
+                        # even though this is an undirected network, we add this so we can make the edges curved
+                        connectionstyle="arc3, rad=0.25",
+                        pos=position,
+                        width=list(edge_widths.values()),
+                        edge_color=range(len(dataset))
                         )
 
 # Make legend based on node sizes
 for n in sorted(node_sizes["weight"].unique()):
-    plt.plot([], [], 'bo', markersize=n/100, label=f"{n}")
-plt.legend(labelspacing=5, loc='center left', bbox_to_anchor=(1,0), frameon=False)
+    plt.plot([], [], 'bo', markersize=n / 100, label=f"{n}")
+plt.legend(labelspacing=5, loc='center left', bbox_to_anchor=(1, 0), frameon=False)
 
 # a legend for edge widths
-lines = np.linspace(start= min(edge_widths.values()), stop= max(edge_widths.values()), num=5) # Get evenly distributed points over range of weights
-from matplotlib.lines import Line2D # Create lines from the weights
-line2ds = [Line2D([],[], linewidth=width, color='black') for width in lines]
+lines = np.linspace(start=min(edge_widths.values()), stop=max(edge_widths.values()),
+                    num=5)  # Get evenly distributed points over range of weights
+from matplotlib.lines import Line2D  # Create lines from the weights
+
+line2ds = [Line2D([], [], linewidth=width, color='black') for width in lines]
 legend2 = plt.legend(line2ds, np.round(lines, decimals=2), bbox_to_anchor=(0, 1))
 
 # SAVE THE GRAPH
