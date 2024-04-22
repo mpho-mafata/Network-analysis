85894af0d6a76ae3c6ba9733d73f066032bd6e99
Mpho Mafata <mafata@sun.ac.za>
2024-04-22 13:52:38 +0200

    improved node and edge attributes

--- network_analysis.py
+++ network_analysis.py
@@ -4,70 +4,93 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-# read the SQL wrangled data
-dataset = pd.read_excel(
-    "/Users/mphomafata/Documents/GitHub/Network-analysis/network-example-data/edgelist_orgs_network.xlsx")
-dataset.columns = ["org1", "org2", "weight"]
-dataset['weight'] = ((dataset['weight']) / max(
-    dataset['weight'])) * 10  # attempt at scaling the edge sizes from zero to one
-# this creates node sizes
-node_sizes_1 = dataset.groupby(['org1']).aggregate({'org1': 'count'})
-node_sizes_2 = dataset.groupby(['org2']).aggregate({'org2': 'count'})
-node_sizes = pd.DataFrame(pd.concat([node_sizes_1, node_sizes_2]))
-node_sizes['orgs'] = node_sizes.index
-node_sizes = node_sizes.groupby(['orgs']).aggregate({'orgs': 'sum'}).reset_index(drop=True)
+# wrangle/ structure the original dataset
+dataset = pd.read_csv("/Users/mphomafata/Documents/GitHub/Network-analysis/network-example-data/orgs_rankings_top.csv",
+                       sep=";",
+                       header=0)
+dataset = dataset[["ut", "organisation"]]
+dataset.columns = ["weight", "target"]
+dataset = pd.merge(left=dataset, right= dataset, how='left', on = 'weight')
+dataset = dataset.query(" target_x != target_y ").reset_index(drop=True, inplace=False)
+dataset['org_array'] = dataset['target_x'] + "," + dataset['target_y']
+dataset["sorted"] = dataset["org_array"].str.split(',').explode().str.split(',').apply(lambda x: [s.lstrip() for s in x]).sort_values().groupby(level=0).agg(lambda x: ' , '.join(map(str, x)))
+dataset = dataset.groupby(["target_x","target_y", "sorted"]).aggregate({'weight': 'count'}).reset_index()
+dataset=dataset.drop_duplicates('sorted').reset_index(drop=True)
 
-# plot the graph
-plt.figure(figsize=(30, 15))
+# SET FREQUENCY THE LIMIT FOR THE EDGES: MINIMUM NUMBER OF SAMPLES AN ATTRIBUTE HAS TO APPEAR
+# dataset = dataset.query(" weight > 70 ")
+
+# CALCULATE THE NODE SIZES HERE AFTER FILTERING FOR THE CUT-OFF WEIGHT
+node_sizes_x = dataset.groupby(["target_x"]).aggregate({'weight': 'sum'}).reset_index()
+node_sizes_y = dataset.groupby(["target_y"]).aggregate({'weight': 'sum'}).reset_index()
+node_sizes = pd.merge(left=node_sizes_x,left_on="target_x",
+                      right=node_sizes_y,right_on="target_y",
+                      how="outer").fillna(0)
+node_sizes["weight"]=node_sizes["weight_x"]+node_sizes["weight_y"]
+node_sizes["weight"] = node_sizes["weight"].astype("int")
+
+# CREATE A RULE FOR THE NODE NAME
+conditions = [(node_sizes["target_x"]==0),
+              (node_sizes["target_x"]!=0)]
+name = [node_sizes["target_y"],
+        node_sizes["target_x"]]
+node_sizes["labels"]=np.select(conditions,name)
+node_sizes = node_sizes[["labels","weight"]]
+
+# SCALE THE EDGE WIDTHS FOR VISUALIZATION
+dataset["weight"] = dataset["weight"]/50
+
+# INITIATE THE GRAPH
+plt.figure(figsize=(10, 10))
 network_graph = nx.Graph()
-network_graph = nx.from_pandas_edgelist(dataset, source="org1", target="org2", edge_attr='weight')
-widths = nx.get_edge_attributes(network_graph, name='weight')
-network_graph = nx.draw(network_graph, with_labels=True, font_size=25, alpha=1.0,
-                        node_size=800,
-                        node_color=range(len(node_sizes)),  # generates random node colours
-                        cmap=plt.cm.viridis,  # colour map for the nodes
-                        arrows=True,
-                        connectionstyle="arc3, rad=0.20",
-                        width=list(widths.values()),
-                        edge_color='grey'
+network_graph = nx.from_pandas_edgelist(dataset, source="target_x", target="target_y", edge_attr ='weight')
+
+# CALCULATE SOME NETWORK METRICS
+from networkx.algorithms import approximation as apxa
+net_centrality = nx.degree_centrality(network_graph)
+print("The network centrality is " + str(len(net_centrality)))
+network_radius = nx.radius(network_graph)
+print("The network radius is " + str(network_radius))
+network_density = nx.density(network_graph)
+print("The density is " + str(network_density))
+net_clusters = apxa.k_components(network_graph)
+print("The number of clusters is " + str(len(net_clusters)))
+
+# SET NODE AND EDGE ATTRIBUTES
+edge_widths = nx.get_edge_attributes(network_graph,"weight")
+new_order = pd.merge(left = pd.DataFrame(network_graph.nodes(data=True)),
+                     left_on=0,
+                     right = node_sizes,
+                     right_on="labels",
+                     how="left")
+node_sizes = new_order[["labels","weight"]]
+
+# DRAW THE NETWORK GRAPH
+position = nx.spring_layout(network_graph, k=4) # Adjust node distances here
+network_graph = nx.draw(network_graph, with_labels=True, font_size=12, alpha=0.75,
+                                node_size = node_sizes["weight"]/1, # adjust the size of nodes here
+                                node_color= range(len(node_sizes)), # random node colours
+                                cmap=plt.cm.Spectral, # colour map for the nodes
+                                arrows=True, # even though this is an undirected network, we add this so we can make the edges curved
+                                connectionstyle="arc3, rad=0.25",
+                                pos=position,
+                                width=list(edge_widths.values()),
+                                edge_color = range(len(dataset))
                         )
+
+# Make legend based on node sizes
+for n in sorted(node_sizes["weight"].unique()):
+    plt.plot([], [], 'bo', markersize=n/100, label=f"{n}")
+plt.legend(labelspacing=5, loc='center left', bbox_to_anchor=(1,0), frameon=False)
+
+# a legend for edge widths
+lines = np.linspace(start= min(edge_widths.values()), stop= max(edge_widths.values()), num=5) # Get evenly distributed points over range of weights
+from matplotlib.lines import Line2D # Create lines from the weights
+line2ds = [Line2D([],[], linewidth=width, color='black') for width in lines]
+legend2 = plt.legend(line2ds, np.round(lines, decimals=2), bbox_to_anchor=(0, 1))
+
+# SAVE THE GRAPH
 plt.savefig(fname='networkx_graph.svg', dpi=800,
             bbox_inches="tight", pad_inches=0.0,
             transparent=True, format="svg")
-
-# wrangle the original dataset
-dataset2 = pd.read_csv("/Users/mphomafata/Documents/GitHub/Network-analysis/network-example-data/orgs_rankings_top.csv",
-                       sep=";",
-                       header=0)
-nodes = dataset2.groupby(["organisation"]).aggregate({'pubs': 'first'}).reset_index()
-edges = dataset2[["ut", "organisation"]]
-# join the dataframe with itself based on the unique identifier (UT)
-edges = pd.merge(left=edges, right=edges, how="left", on='ut')
-# then remove the duplicates where org x is the same as org y
-edges = edges.query(" organisation_x != organisation_y ").reset_index(drop=True, inplace=False)
-# now remove the duplicates where org x: orgy is the same as org y: org x
-# first create an array of organization_x and organization_y and add it to the dataframe
-edges["org_array"] = edges["organisation_x"] + "," + edges["organisation_y"]
-edges["sorted"] = edges["org_array"].str.split(',').explode().str.split(',').apply(
-    lambda x: [s.lstrip() for s in x]).sort_values().groupby(level=0).agg(lambda x: ' , '.join(map(str, x)))
-# now count the unique number of UTs
-edges = edges.groupby(["organisation_x", "organisation_y", "sorted"]).aggregate({'ut': 'count'}).reset_index()
-edges['ut'] = ((edges['ut']) / max(edges['ut'])) * 10  # attempt at scaling the edge sizes from zero to one
-
-# plot the graph
-plt.figure(figsize=(30, 15))
-network_graph = nx.Graph()
-network_graph = nx.from_pandas_edgelist(edges, source="organisation_x", target="organisation_y", edge_attr='ut')
-widths = nx.get_edge_attributes(network_graph, name='ut')
-network_graph = nx.draw(network_graph, with_labels=True, font_size=25, alpha=1.0,
-                        node_size=800,
-                        node_color=range(len(nodes)),  # generates random node colours
-                        cmap=plt.cm.viridis,  # colour map for the nodes
-                        arrows=True,
-                        connectionstyle="arc3, rad=0.20",
-                        width=list(widths.values()),
-                        edge_color='grey'
-                        )
-plt.savefig(fname='networkx_graph_2.svg', dpi=800,
-            bbox_inches="tight", pad_inches=0.0,
-            transparent=True, format="svg")
+plt.show()
